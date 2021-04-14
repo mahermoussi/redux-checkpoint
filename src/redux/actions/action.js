675db4cb387a7c8fd98d019cd22c6d174cd7c560
maher-moussi <mahermoussi777@gmail.com>
2021-04-14 15:59:18 +0200

    first commit

---
+++ src/redux/actions/action.js
@@ -0,0 +1,12 @@
+import {ADD , REMOVE, EDIT} from './actionTypes'
+
+
+export const add = (newtask) =>{
+    return {type:ADD ,payload:newtask}
+}
+export const remove = (id) =>{
+    return {type:REMOVE ,payload:id}
+}
+export const edit =(id ,newvalue)=> {
+    return {type:EDIT ,payload:{id,newvalue}}
+}
---
+++ src/redux/reducer.js
@@ -0,0 +1,14 @@
+import {ADD ,REMOVE,EDIT} from './actions/actionTypes'
+
+const initialState ={
+    todolist:[]
+}
+const reducer = (state=initialState ,action) => {
+    switch (action.type){
+        case ADD : return {todolist : [...state.todolist, action.payload]}
+        case REMOVE : return {todolist : state.todolist.filter((el,id)=> id !==action.payload)}
+        case EDIT : return {todolist : state.todolist.map((el,id)=> id === action.payload.id ? el =action.payload.newvalue :el)}
+        default : return state
+    }
+}
+export default reducer
